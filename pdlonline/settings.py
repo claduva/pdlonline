02b64b61d5b5a2213f2dfe790386f98be06d4cca
Colton Ladbury <cladbury@gmail.com>
2021-03-04 21:37:15 -0800

    Update settings

--- pdlonline/settings.py
+++ pdlonline/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'pokemondraftleague/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'pdlonline/staticfiles')
 STATIC_URL = '/static/' 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
