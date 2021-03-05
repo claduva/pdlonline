b7cca66918b2926da24f140e20309d55123e8cbf
Colton Ladbury <cladbury@gmail.com>
2021-03-05 13:28:26 -0800

    General updates

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
-if settings.DEBUG == True:
-    from pdlonline.configuration import *
+#if settings.DEBUG == True:
+#    from pdlonline.configuration import *
 import psycopg2
 
 from pokemon.models import pokemon
