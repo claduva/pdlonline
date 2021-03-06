872580c21b4f75c9d9a1bca00709782f86204eaa
Colton Ladbury <cladbury@gmail.com>
2021-03-05 18:59:58 -0800

    Update Pokedex

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [ 
     path("", views.home, name="home"),
     path("runscript/", views.runscript, name="runscript"),
+    path("update_all_pokemon/", views.update_all_pokemon, name="update_all_pokemon"),
 ]
--- pokemon/views.py
+++ pokemon/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render,redirect
-from .models import pokemon
+from .models import pokemon, pokemon_basestats
 
 # Create your views here.
 def pokedex(request):
     context = {
-        'all_pokemon': pokemon.objects.all(),
+        'all_pokemon': pokemon.objects.all().order_by("pokedex_number","name"),
     }
     return  render(request,'pokedex.html',context)
