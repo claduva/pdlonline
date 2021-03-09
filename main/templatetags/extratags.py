9a6b8e9d2779087589f0517e234a63a01b59dbc5
Colton Ladbury <cladbury@gmail.com>
2021-03-08 16:36:00 -0800

    Update pokedex

--- main/templatetags/extratags.py
+++ main/templatetags/extratags.py
@@ -1,6 +1,15 @@
 from django import template
 register = template.Library()
 
+@register.filter(name='pokedexnumber')
+def pokedexnumber(int):
+    if int<10:
+        return "00"+str(int)
+    elif int<100:
+        return "0"+str(int)
+    else:
+        return str(int)
+
 @register.filter(name='replace_spaces')
 def replace_spaces(string):
     return string.replace(" ","_")
--- pokemon/views.py
+++ pokemon/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render,redirect
-from .models import pokemon, pokemon_basestats
+from .models import pokemon, pokemon_basestats, pokemon_type
 
 # Create your views here.
 def pokedex(request):
+    types=['-Type-']+list(pokemon_type.objects.all().order_by('type').distinct('type').values_list('type',flat=True))
+    print(types)
     context = {
         'all_pokemon': pokemon.objects.all().order_by("pokedex_number","name"),
+        'types': types,
     }
     return  render(request,'pokedex.html',context)
 
