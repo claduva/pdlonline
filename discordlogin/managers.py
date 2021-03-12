392f130344afcfe9aa3e15199ac7883d614f710f
harbar20 <harbar2021@gmail.com>
2021-03-11 21:48:19 -0500

    got auth to add to db and login successfully

--- discordlogin/managers.py
+++ discordlogin/managers.py
@@ -2,13 +2,14 @@ from django.contrib.auth import models
 
 class DiscordUserManager(models.UserManager):
     def create_new_discord_user(self, user):
-        new_user = self.create_user(
+        new_user = self.create(
             id=user["id"], 
             avatar=user["avatar"], 
             public_flags=user["public_flags"], 
             flags=user["flags"], 
             locale=user["locale"], 
             mfa_enabled=user["mfa_enabled"], 
-            discord_tag=f'{user["username"]}#{user["discriminator"]}'
+            discord_tag=f'{user["username"]}#{user["discriminator"]}',
         )
+        print(new_user);
         return new_user
