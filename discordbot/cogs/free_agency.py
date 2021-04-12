fd58a8a2b372f0528b240d6c4954964875ad18b1
Colton Ladbury <cladbury@gmail.com>
2021-04-12 12:30:23 -0700

    update bot

--- discordbot/cogs/free_agency.py
+++ discordbot/cogs/free_agency.py
@@ -15,39 +15,42 @@ class FreeAgency(commands.Cog):
             await asyncio.sleep(30)
             fa_data=requests.get(f'{baseurl}free_agency/').json()
             for item in fa_data:
-                #fadata
-                faid=item['id']
-                subleagueid=item['team']['season']['subleague']['id']
-                leagueid=item['team']['season']['subleague']['league']['id']
-                teamname=item['team']['teamname']
-                teamabbreviation=item['team']['teamabbreviation']
-                logo=item['team']['logo']
-                addedpokemon=item['added_pokemon']['name']
-                sprite=item['added_pokemon']['sprite']
-                droppedpokemon=item['dropped_pokemon']['name']
-                weekeffective=item['weekeffective']
-                #get channel data
-                fadata=requests.get(f'{baseurl}discord_settings/{subleagueid}/').json()
-                server=fadata['server']
-                fachannel=fadata['fachannel']
-                #get guild
-                goi=get(self.bot.guilds,id=server)
-                channel=get(goi.channels,id=fachannel)
-                embed=discord.Embed(
-                    title=f"The {teamname} ({teamabbreviation}) have picked up {addedpokemon} in free agency. To make room on the roster, they have released {droppedpokemon}.", 
-                    url=f"https://pokemondraftleagueonline.herokuapp.com/league/{leagueid}/subleague/{subleagueid}/free_agency/", 
-                    description=f"This will be effective Week {weekeffective}. Go to https://pokemondraftleagueonline.herokuapp.com/league/{leagueid}/subleague/{subleagueid}/free_agency/ for more information.")
-                embed.set_thumbnail(url=logo)
-                embed.set_image(url=sprite)
-                #embed.timestamp = datetime.datetime.utcnow()
-                await channel.send(embed=embed)
-                url=f'{baseurl}free_agency/{faid}/'
-                data={'announced':True}
-                update=requests.put(url,data = data)
-                if update.status_code == 200:
-                    print("updated")
-                else:
-                    print("error")
+                try:
+                    #fadata
+                    faid=item['id']
+                    subleagueid=item['team']['season']['subleague']['id']
+                    leagueid=item['team']['season']['subleague']['league']['id']
+                    teamname=item['team']['teamname']
+                    teamabbreviation=item['team']['teamabbreviation']
+                    logo=item['team']['logo']
+                    addedpokemon=item['added_pokemon']['name']
+                    sprite=item['added_pokemon']['sprite']
+                    droppedpokemon=item['dropped_pokemon']['name']
+                    weekeffective=item['weekeffective']
+                    #get channel data
+                    fadata=requests.get(f'{baseurl}discord_settings/{subleagueid}/').json()
+                    server=fadata['server']
+                    fachannel=fadata['fachannel']
+                    #get guild
+                    goi=get(self.bot.guilds,id=server)
+                    channel=get(goi.channels,id=fachannel)
+                    embed=discord.Embed(
+                        title=f"The {teamname} ({teamabbreviation}) have picked up {addedpokemon} in free agency. To make room on the roster, they have released {droppedpokemon}.", 
+                        url=f"https://pokemondraftleagueonline.herokuapp.com/league/{leagueid}/subleague/{subleagueid}/free_agency/", 
+                        description=f"This will be effective Week {weekeffective}. Go to https://pokemondraftleagueonline.herokuapp.com/league/{leagueid}/subleague/{subleagueid}/free_agency/ for more information.")
+                    embed.set_thumbnail(url=logo)
+                    embed.set_image(url=sprite)
+                    #embed.timestamp = datetime.datetime.utcnow()
+                    await channel.send(embed=embed)
+                    url=f'{baseurl}free_agency/{faid}/'
+                    data={'announced':True}
+                    update=requests.put(url,data = data)
+                    if update.status_code == 200:
+                        print("updated")
+                    else:
+                        print("error")
+                except Exception as e:
+                    print(e)
 
 def setup(bot):
     bot.add_cog(FreeAgency(bot))
--- discordbot/cogs/trading.py
+++ discordbot/cogs/trading.py
@@ -15,39 +15,42 @@ class Trading(commands.Cog):
             await asyncio.sleep(30)
             trade_data=requests.get(f'{baseurl}trading/').json()
             for item in trade_data:
-                #tradedata
-                tradeid=item['id']
-                subleagueid=item['team']['season']['subleague']['id']
-                leagueid=item['team']['season']['subleague']['league']['id']
-                teamname=item['team']['teamname']
-                teamabbreviation=item['team']['teamabbreviation']
-                logo=item['team']['logo']
-                addedpokemon=item['added_pokemon']['name']
-                sprite=item['added_pokemon']['sprite']
-                droppedpokemon=item['dropped_pokemon']['name']
-                weekeffective=item['weekeffective']
-                #get channel data
-                tradedata=requests.get(f'{baseurl}discord_settings/{subleagueid}/').json()
-                server=tradedata['server']
-                tradechannel=tradedata['tradechannel']
-                #get guild
-                goi=get(self.bot.guilds,id=server)
-                channel=get(goi.channels,id=tradechannel)
-                embed=discord.Embed(
-                    title=f"In the latest blockbuster trade, the {teamname} ({teamabbreviation}) have picked up {addedpokemon} in exchange for their {droppedpokemon}.", 
-                    url=f"https://pokemondraftleagueonline.herokuapp.com/league/{leagueid}/subleague/{subleagueid}/trading/", 
-                    description=f"This will be effective Week {weekeffective}. Go to https://pokemondraftleagueonline.herokuapp.com/league/{leagueid}/subleague/{subleagueid}/trading/ for more information.")
-                embed.set_thumbnail(url=logo)
-                embed.set_image(url=sprite)
-                #embed.timestamp = datetime.datetime.utcnow()
-                await channel.send(embed=embed)
-                url=f'{baseurl}trading/{tradeid}/'
-                data={'announced':True}
-                update=requests.put(url,data = data)
-                if update.status_code == 200:
-                    print("updated")
-                else:
-                    print("error")
+                try:
+                    #tradedata
+                    tradeid=item['id']
+                    subleagueid=item['team']['season']['subleague']['id']
+                    leagueid=item['team']['season']['subleague']['league']['id']
+                    teamname=item['team']['teamname']
+                    teamabbreviation=item['team']['teamabbreviation']
+                    logo=item['team']['logo']
+                    addedpokemon=item['added_pokemon']['name']
+                    sprite=item['added_pokemon']['sprite']
+                    droppedpokemon=item['dropped_pokemon']['name']
+                    weekeffective=item['weekeffective']
+                    #get channel data
+                    tradedata=requests.get(f'{baseurl}discord_settings/{subleagueid}/').json()
+                    server=tradedata['server']
+                    tradechannel=tradedata['tradechannel']
+                    #get guild
+                    goi=get(self.bot.guilds,id=server)
+                    channel=get(goi.channels,id=tradechannel)
+                    embed=discord.Embed(
+                        title=f"In the latest blockbuster trade, the {teamname} ({teamabbreviation}) have picked up {addedpokemon} in exchange for their {droppedpokemon}.", 
+                        url=f"https://pokemondraftleagueonline.herokuapp.com/league/{leagueid}/subleague/{subleagueid}/trading/", 
+                        description=f"This will be effective Week {weekeffective}. Go to https://pokemondraftleagueonline.herokuapp.com/league/{leagueid}/subleague/{subleagueid}/trading/ for more information.")
+                    embed.set_thumbnail(url=logo)
+                    embed.set_image(url=sprite)
+                    #embed.timestamp = datetime.datetime.utcnow()
+                    await channel.send(embed=embed)
+                    url=f'{baseurl}trading/{tradeid}/'
+                    data={'announced':True}
+                    update=requests.put(url,data = data)
+                    if update.status_code == 200:
+                        print("updated")
+                    else:
+                        print("error")
+                except Exception as e:
+                    print(e)
 
 def setup(bot):
     bot.add_cog(Trading(bot))
