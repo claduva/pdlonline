7847f12bf66e38b835cab1b381ac3e2a3f6f5a99
claduva <cladbury@gmail.com>
2021-05-25 17:46:57 -0700

    update

--- app_api/views.py
+++ app_api/views.py
@@ -32,7 +32,7 @@ class UserTeamList(generics.ListAPIView):
     serializer_class = CoachSerializer
     def get_queryset(self):
         user_id = self.kwargs['user_id']
-        return coach.objects.filter(user__discordid=user_id)
+        return coach.objects.filter(user__discordid=user_id)[0:5]
 
 class UpcomingMatchList(generics.ListAPIView):
     serializer_class = MatchSerializer
