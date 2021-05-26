4ecda880bcce1a2e2f548cd296821541000fa5a3
claduva <cladbury@gmail.com>
2021-05-25 22:04:20 -0700

    update

--- app_api/views.py
+++ app_api/views.py
@@ -44,4 +44,4 @@ class LeagueTierSet(generics.ListAPIView):
     serializer_class = TierSerializer
     def get_queryset(self):
         subleague_id = self.kwargs['subleague_id']
-        return league_pokemon.objects.filter(subleague__id=subleague_id)
+        return league_pokemon.objects.filter(subleague__id=subleague_id).exclude(tier__tier="Banned")
