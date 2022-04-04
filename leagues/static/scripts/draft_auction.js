e638e5f4aad7420b1060c3e8510f4bbd97b89e86
claduva <cladbury@gmail.com>
2022-04-04 11:37:39 -0700

    update auction draft

--- leagues/static/scripts/draft_auction.js
+++ leagues/static/scripts/draft_auction.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
- 
+    console.log("draft_auction.js loaded");
+    $( "#search" ).change(function() {
+        st=$( "#search" ).val();
+        if (st==""){
+            $(".nobidrow").removeClass("d-none");
+        } else{
+            $(".nobidrow").addClass("d-none");
+            $(".nobidrow").filter(function(){return $(this).find(".nobidname").text().toLowerCase().includes(st)}).removeClass("d-none")
+        }
+      });
 })
 
