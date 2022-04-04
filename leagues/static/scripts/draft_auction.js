128f97046760b0c1c2a462c5b7836b85a3b6d14f
claduva <cladbury@gmail.com>
2022-04-04 12:37:42 -0700

    update auction draft

--- leagues/static/scripts/draft_auction.js
+++ leagues/static/scripts/draft_auction.js
@@ -8,6 +8,23 @@ $(document).ready(function() {
             $(".nobidrow").addClass("d-none");
             $(".nobidrow").filter(function(){return $(this).find(".nobidname").text().toLowerCase().includes(st)}).removeClass("d-none")
         }
-      });
+    });
+    
+    var x = setInterval(function() {
+        $(".timerem").each(function(){
+            var timestamp=parseInt($(this).find(".picktime").text());
+            var d = new Date();
+            var seconds = Math.round(d.getTime() / 1000);   // Unix time in seconds
+            var secondsrem=parseInt($("#timer").text())*60*60-(seconds-timestamp);
+            var hours = Math.floor(secondsrem / 3600);
+            var minutes = Math.floor((secondsrem-hours*3600) / 60);
+            var seconds = Math.floor((secondsrem - hours*3600 - minutes*60));
+            $(this).find(".timeleft").html(hours+" hr<br>"+minutes+" min<br>"+seconds+" sec");
+        })
+    }, 1000);
+
+    var x = setInterval(function() {
+        location.reload();
+    }, 1000*60*15);
 })
 
